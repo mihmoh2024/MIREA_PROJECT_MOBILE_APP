8b658b84397213ab04eab670e944ff0bde695771
mihmoh <mihmohpihpoh07@gmail.com>
2025-03-03 11:38:37 +0300

    end 03.03.2025

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lesson2"
+include(":app")
+include(":app:activitylifecycle")
+include(":app:activitylifecycle2")
+include(":app:multiactivity")
+include(":app:intentfilter")
