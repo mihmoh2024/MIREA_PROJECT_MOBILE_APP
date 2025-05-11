77d2a09b7b34c8177fb5d7bf57924ec1d2f868d3
mihmoh <mihmohpihpoh07@gmail.com>
2025-05-12 00:38:50 +0300

    12.05

--- app/src/main/java/ru/mirea/smirnovmv/mireaproject/AudioFragment.java
+++ app/src/main/java/ru/mirea/smirnovmv/mireaproject/AudioFragment.java
@@ -1,64 +1,148 @@
 package ru.mirea.smirnovmv.mireaproject;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AudioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.IOException;
+
+import ru.mirea.smirnovmv.mireaproject.databinding.FragmentAudioBinding;
+
 public class AudioFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AudioFragment() {
-        // Required empty public constructor
+    private enum State{
+        IDLE,
+        RECORD,
+        PLAYING
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AudioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AudioFragment newInstance(String param1, String param2) {
+    private FragmentAudioBinding binding = null;
+    private String recordFilePath = null;
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
+    private State state = State.IDLE;
+
+    @NonNull
+    public static AudioFragment newInstance() {
         AudioFragment fragment = new AudioFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentAudioBinding.inflate(inflater, container, false);
+        binding.recordVoiceButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        OnRecordButtonClicked();
+                    }
+                }
+        );
+        binding.playVoiceButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        OnPlayButtonClicked();
+                    }
+                }
+        );
+
+        recordFilePath = (new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_MUSIC),
+                "/audiorecordtest.3gp")).getAbsolutePath();
+        return binding.getRoot();
+    }
+
+    private void OnRecordButtonClicked() {
+        switch (state) {
+            case IDLE: {
+                StartRecord();
+                break;
+            }
+            case RECORD: {
+                StopRecord();
+                break;
+            }
+            case PLAYING: { break; }
+            default: { break; }
+        }
+    }
+    private void OnPlayButtonClicked() {
+        switch (state) {
+            case IDLE: {
+                StartPlaying();
+                break;
+            }
+            case PLAYING: {
+                StopPlaying();
+                break;
+            }
+            case RECORD: { break; }
+            default: { break; }
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio, container, false);
+
+    public void StartPlaying() {
+        if(state == State.PLAYING) return;
+        state = State.PLAYING;
+        binding.recordVoiceButton.setEnabled(false);
+        binding.playVoiceButton.setEnabled(true);
+
+        try	{
+            player = new MediaPlayer();
+            player.setDataSource(recordFilePath);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e("Work", "prepare() failed");
+        }
+    }
+    public void StopPlaying() {
+        if(state == State.IDLE) return;
+        state = State.IDLE;
+        binding.recordVoiceButton.setEnabled(true);
+        binding.playVoiceButton.setEnabled(true);
+
+        player.release();
+        player = null;
+    }
+    public void StartRecord() {
+        if(state == State.RECORD) return;
+        state = State.RECORD;
+        binding.recordVoiceButton.setEnabled(true);
+        binding.playVoiceButton.setEnabled(false);
+
+        try	{
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setOutputFile(recordFilePath);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.prepare();
+            recorder.start();
+        } catch (IOException e)	{
+            Log.e("Work",	"prepare()	failed");
+        }
+    }
+    public void StopRecord() {
+        if(state == State.IDLE) return;
+        state = State.IDLE;
+        binding.recordVoiceButton.setEnabled(true);
+        binding.playVoiceButton.setEnabled(true);
+
+        recorder.stop();
+        recorder.release();
+        recorder = null;
     }
 }
--- app/src/main/java/ru/mirea/smirnovmv/mireaproject/CameraFragment.java
+++ app/src/main/java/ru/mirea/smirnovmv/mireaproject/CameraFragment.java
@@ -1,146 +1,83 @@
 package ru.mirea.smirnovmv.mireaproject;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
-public class CameraFragment extends Fragment {
+import ru.mirea.smirnovmv.mireaproject.databinding.FragmentCameraBinding;
 
-    private static final int REQUEST_CODE_PERMISSION = 100;
-    private boolean isWork = false;
-    private Uri imageUri;
-    private ImageView imageView1;
-    private ImageView lastClickedView;
+public class CameraFragment extends Fragment implements ActivityResultCallback<ActivityResult> {
+    private ActivityResultLauncher<Intent> cameraActivityResultLauncher = null;
+    private FragmentCameraBinding binding = null;
+    private Uri store_profile_photo_uri;
+    public CameraFragment() { }
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public CameraFragment() {}
-
-    public static CameraFragment newInstance(String param1, String param2) {
+    @NonNull
+    public static CameraFragment newInstance() {
         CameraFragment fragment = new CameraFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
-        imageView1 = view.findViewById(R.id.imageView1);
-
-        checkPermissions();
-
-        ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
+        Log.i("Work", "onCreateView");
+        binding = FragmentCameraBinding.inflate(inflater, container, false);
+        cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), this);
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
                     @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            if (lastClickedView.equals(imageView1)) {
-                                imageView1.setImageURI(imageUri);
-                            }
-                        }
+                    public void onClick(View view) {
+                        OnUpdatePhotoButtonClicked(view);
                     }
-                });
-
-        imageView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isWork) {
-                    lastClickedView = imageView1;
-                    openCameraFirst(cameraActivityResultLauncher);
                 }
-            }
-        });
+        );
 
-        return view;
+        return binding.getRoot();
+    }
+    private Uri getStorageFileUri() throws IOException {
+        File storageDirectory = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File file = File.createTempFile("temp_user_profile_photo", ".jpg", storageDirectory);
+        String authorities = requireContext().getPackageName() + ".fileprovider";
+        return FileProvider.getUriForFile(getActivity(), authorities, file);
     }
 
-    private void checkPermissions() {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-            }, REQUEST_CODE_PERMISSION);
-        } else {
-            isWork = true;
-        }
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            isWork = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
-        }
-    }
-
-    private void openCameraFirst(ActivityResultLauncher<Intent> cameraActivityResultLauncher) {
+    public void OnUpdatePhotoButtonClicked(View v) {
         try {
-            File photoFile = createImageFile(true); // First image
-            String authorities = getActivity().getApplicationContext().getPackageName() + ".fileprovider";
-            imageUri = FileProvider.getUriForFile(requireContext(), authorities, photoFile);
-
+            store_profile_photo_uri = getStorageFileUri();
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, store_profile_photo_uri);
             cameraActivityResultLauncher.launch(cameraIntent);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
-
-    private File createImageFile(boolean firstImage) throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
-        String imageFileName = "IMAGE_" + timeStamp + "_";
-
-        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File imageDir = new File(storageDirectory, firstImage ? "MyAppImages1" : "MyAppImages2");
-        if (!imageDir.exists()) {
-            imageDir.mkdirs();
-        }
-
-        return File.createTempFile(imageFileName, ".jpg", imageDir);
+    @Override
+    public void onActivityResult(ActivityResult o) {
+        if(o.getResultCode() == Activity.RESULT_OK)
+            binding.profilePhotoView.setImageURI(store_profile_photo_uri);
     }
 }
